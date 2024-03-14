90121d7837217cf1376ea030743610f481fb156f
BoevoyEzhik <nikita120597@gmail.com>
2024-03-14 16:59:20 +0300

    Исправил тесты

--- findneighbors/find_neighbors/tests/test_create_user.py
+++ findneighbors/find_neighbors/tests/test_create_user.py
@@ -5,31 +5,25 @@ from find_neighbors.models import Users
 
 class SomeTest(TestCase):
 
-    @classmethod
-    def setUpTestData(cls):
-        Users.objects.create(name='Name', x=20, y=20)
-
     def test_create_user_get(self):
         url = reverse('create-user')
         response = self.client.get(path=url, content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
     def test_create_user_post_status_code_201(self):
-        self.user = Users.objects.get(name='Name')
         url = reverse('create-user')
-        data = {'name': self.user.name, 'x': self.user.x, 'y': self.user.y}
+        data = {'name': 'test_user', 'x': -23.5, 'y': 5}
         response = self.client.post(path=url, data=data, content_type='application/json')
         self.assertEqual(response.status_code, 201)
 
     def test_create_user_successful_db_creation(self):
-        self.user = Users.objects.get(name='Name')
         url = reverse('create-user')
-        data = {'name': self.user.name, 'x': self.user.x, 'y': self.user.y}
+        data = {'name': 'test_user2', 'x': -23.5, 'y': 5}
         response = self.client.post(path=url, data=data, content_type='application/json')
-        self.assertEqual(self.user.name, 'Name')
+        user = Users.objects.get(name='test_user2')
+        self.assertEqual(user.name, 'test_user2')
 
     def test_create_user_post_negative(self):
-        self.user = Users.objects.create(name='Name', x=20, y=20)
         url = reverse('create-user')
         data = {'name': 3, 'x': 'we', 'y': 20}
         response = self.client.post(path=url, data=data, content_type='application/json')
